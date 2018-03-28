598339c2f18b946ed1fcafbb4dfebfdf63526946
Dimmy Magalhães <dimmyk@gmail.com>
2018-03-28 11:55:14 -0300

    Initial Commit

---
+++ lib/rmetaheuristics.rb
@@ -0,0 +1,5 @@
+require "rmetaheuristics/version"
+
+module Rmetaheuristics
+  # Your code goes here...
+end
---
+++ spec/rmetaheuristics_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rmetaheuristics do
+  it "has a version number" do
+    expect(Rmetaheuristics::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
